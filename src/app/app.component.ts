826f4b783b01d1d9b39ba7b103d2c234a0fe55ac
Carlos <arlo252@gmail.com>
2023-03-31 19:00:36 -0400

    Step 7 - Create the Home Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-azores-app';
+  // title = 'my-azores-app';
+  myName = 'Carlos Sousa'
+  
 }
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  azoresUrl: string = "https://image.arrivalguides.com/1500x600/18/25bf0721b8426922ae087e1d397e58b8.jpg"
+}
