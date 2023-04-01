98b8b460b095ba597f689220376616734916636f
Carlos <arlo252@gmail.com>
2023-04-01 18:31:09 -0400

    add Island info

---
+++ src/app/components/islands/islands-info-data.ts
@@ -0,0 +1,8 @@
+import { islandinfo } from "./island-info";
+
+export const Islandsinfo: islandinfo[]=[
+    {name:"St-Miguel", attractionSites:['Ponta Delgada', 'Furnas', 'Moms House']},
+    {name:"Pico", attractionSites:['Ponta do Pico', 'Gruta das Torres', 'Whale Museum']},
+    {name:"Terceira", attractionSites:['Algar do Carvao', 'Terceira Garden', 'Grandmas House']},
+    {name:"Corvo", attractionSites:['The Crater', 'Moinhos Do Corvo', 'Dads House']},
+]
---
+++ src/app/components/islands/islands.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-islands',
+  templateUrl: './islands.component.html',
+  styleUrls: ['./islands.component.css']
+})
+export class IslandsComponent {
+
+}
