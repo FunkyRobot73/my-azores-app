5fee72035c5a1bea19904b9532b02e0424cd776a
Carlos <arlo252@gmail.com>
2023-04-02 16:19:17 -0400

    Step 10 - Display Properties

--- src/app/components/islands/islands.component.ts
+++ src/app/components/islands/islands.component.ts
@@ -8,9 +8,19 @@ import { GetislandsService } from 'src/app/services/getislands.service';
 })
 export class IslandsComponent implements OnInit {
   islands;
+  color;
   constructor(private _islands: GetislandsService) { }
 
   ngOnInit() {
     this.islands=this._islands.getIslands();
+    this.color=this.colorit();
+  }
+  colorit(){
+    for (let i = 0; i < this.islands.length; i++) {
+      if (this.islands[i].attractionSites.length<3) {
+        return false;
+      }
+    }
+    return true;
   }
 }
