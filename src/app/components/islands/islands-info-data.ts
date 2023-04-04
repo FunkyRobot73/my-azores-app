bac252cc49c35493b0f8ed7225018463a3ecc9e9
Carlos <arlo252@gmail.com>
2023-04-04 19:58:37 -0400

    Weather Update

--- src/app/components/islands/islands-info-data.ts
+++ src/app/components/islands/islands-info-data.ts
@@ -1,4 +1,5 @@
 import { islandinfo } from "./islands-info";
+import { islandsWeather } from "./"
 
 export const Islandsinfo: islandinfo[]=[
     {name:"St-Miguel", attractionSites:['Ponta Delgada', 'Furnas', 'Moms House']},
@@ -10,7 +11,7 @@ export const Islandsinfo: islandinfo[]=[
 
 export const IslandsWeather: islandsWeather[]=[
     {name:"St-Miguel", springN:23, springM:43, summerN:22, summerM:43, winterN:12, winterM:25, autumnN:22, autumnM:36, visitFrom:"October", visitTo:"April"},
-    {name:"Pico", springN:23, springM:40, summerN:28, summerM:43, winterN:12, winterM:25, autumnN:22, autumnM:36, visitFrom:"September", visitTo:"May"},
-    {name:"Terceira", springN:23, springM:40, summerN:28, summerM:43, winterN:12, winterM:25, autumnN:22, autumnM:36, visitFrom:"August", visitTo:"March"},
-    {name:"Corvo", springN:23, springM:40, summerN:28, summerM:43, winterN:12, winterM:25, autumnN:22, autumnM:36, visitFrom:"January", visitTo:"December"}
+    {name:"Pico", springN:23, springM:30, summerN:28, summerM:33, winterN:12, winterM:25, autumnN:22, autumnM:36, visitFrom:"September", visitTo:"May"},
+    {name:"Terceira", springN:23, springM:40, summerN:28, summerM:33, winterN:12, winterM:25, autumnN:22, autumnM:36, visitFrom:"August", visitTo:"March"},
+    {name:"Corvo", springN:23, springM:40, summerN:28, summerM:23, winterN:12, winterM:25, autumnN:22, autumnM:32, visitFrom:"January", visitTo:"December"}
 ]
